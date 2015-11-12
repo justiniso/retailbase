df7a9de79c63acd2b25a58dea7d16febc1a6dfaf
Justin Iso <justin.james.iso@gmail.com>
2015-11-11 23:59:44 -0500

    stable version of static; rename assets to static to allow apache to serve static files without touching flask

--- src/frontend/__init__.py
+++ src/frontend/__init__.py
@@ -2,38 +2,19 @@
 
 import os
 
-from webassets import Bundle
-from flask.ext.assets import Environment
-
 from . import blueprints
 from src import appfactory
 
 
 def create_frontend_app():
     template_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-    static_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'assets')
-
-    # css_bundle = Bundle(
-    #     'build/css/*.css',
-    #     output='../../static/main.css')
-    #
-    # js_bundle = Bundle(
-    #     'build/js/*.js',
-    #     output='../../static/main.js')
-    #
-    # fonts_bundle = Bundle(
-    #     'fonts/bootstrap/*')
+    static_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'static')
 
     app = appfactory.create_app(
         'frontend',
         blueprints=blueprints,
         static_folder=static_folder,
         template_folder=template_folder)
-
-    # env = Environment(app)
-    # env.register('css_main', css_bundle)
-    # env.register('js_main', js_bundle)
-    # env.register('fonts_bundle', fonts_bundle)
 
     return app
 
