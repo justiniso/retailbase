09dc2b55bc15b225e5e3fc40937ff9100c622f1f
Justin Iso <justin.james.iso@gmail.com>
2015-11-12 20:35:52 -0500

    try to optimize styles and static a bit to make them smaller, fix bug with requirejs

--- src/staticapp/config.rb
+++ src/staticapp/config.rb
@@ -9,8 +9,7 @@ sass_dir = "src/sass"
 images_dir = "build/images"
 javascripts_dir = "build/js"
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
