12e930c0cd54153125bafe52e9d5c5c7ee460a67
Justin Iso <justin.james.iso@gmail.com>
2015-11-16 21:04:02 -0500

    favicon and rearrange blueprints

--- src/frontend/blueprints/base.py
+++ src/frontend/blueprints/base.py
@@ -1,30 +1,11 @@
 # -*- coding: utf-8 -*-
-from collections import OrderedDict
-
 from flask import Blueprint, request, render_template, jsonify, send_from_directory
-from src.api.model.post import Post
+
 
 bp = Blueprint(
-    'home',
+    'base',
     __name__
 )
-
-
-@bp.route('/', methods=['GET'])
-def home(_request=request):
-
-    content_count = 3
-    featured_tags = ['stocking-stuffer', 'rush', 'gadgets', 'star wars', 'foodie', 'her', 'him',
-                     'alcohol', 'photographer', 'kids', 'hipster', 'coworker']
-    featured_posts = OrderedDict()
-
-    for tag in featured_tags:
-        posts = Post.objects(tags=tag)
-        featured_posts[tag] = posts[max(0, len(posts) - content_count):len(posts)]
-
-    all_tags = Post.objects.distinct('tags')
-    return render_template('home.html', featured_posts=featured_posts, all_tags=all_tags)
-
 
 @bp.route('/healthcheck')
 def healthcheck(_request=request):
---
+++ src/frontend/blueprints/home.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from collections import OrderedDict
+
+from flask import Blueprint, request, render_template
+from src.api.model.post import Post
+
+
+bp = Blueprint(
+    'home',
+    __name__
+)
+
+@bp.route('/', methods=['GET'])
+def home(_request=request):
+
+    content_count = 3
+    featured_tags = ['stocking-stuffer', 'rush', 'gadgets', 'star wars', 'foodie', 'her', 'him',
+                     'alcohol', 'photographer', 'kids', 'hipster', 'coworker']
+    featured_posts = OrderedDict()
+
+    for tag in featured_tags:
+        posts = Post.objects(tags=tag)
+        featured_posts[tag] = posts[max(0, len(posts) - content_count):len(posts)]
+
+    all_tags = Post.objects.distinct('tags')
+    return render_template('home.html', featured_posts=featured_posts, all_tags=all_tags)
