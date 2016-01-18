38804383a5ab5d5a6befef7b73d7ae8890d5b91a
Justin Iso <justin.james.iso@gmail.com>
2016-01-18 00:01:58 -0500

    update provisioning and touch up product data

--- provisioning/initialize.sh
+++ provisioning/initialize.sh
@@ -9,6 +9,8 @@ apt-get install fail2ban
 
 useradd -r -m -s /bin/bash deploy
 
+chmod 777 /var/log
+
 # Python
 apt-get install -y python-setuptools
 easy_install -U pip
@@ -20,3 +22,16 @@ apt-get install -y git
 cd /home/deploy
 git clone git@github.com:justiniso/retailbase.git
 
+apt-add-repository ppa:nginx/stable
+apt-get update
+apt-get install -y nginx
+ln -nsf /home/deploy/retailbase/provisioning/etc/nginx/conf.d/app.conf /etc/nginx/conf.d/app.conf
+
+# COMMENT OUT THE LINE IN nginx.conf THAT SAYS include /etc/nginx/sites-enabled/*;
+
+
+apt-get install -y mongodb
+mongod
+
+cd /home/deploy
+gunicorn -D -b 0.0.0.0:8000 application:application
